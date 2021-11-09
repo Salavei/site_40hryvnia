d77786c0876d1bd30af19abc5f8f5eec54080bf4
Salavei <kano.mori@mail.ru>
2021-11-09 04:29:23 +0300

    ‘migrating_to_flask,_api_added’

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, flash, url_for
+import json
+import requests
+
+api = 'f89fed0d4d207b879a2d'
+
+byn = requests.get(f'https://free.currconv.com/api/v7/convert?q=UAH_BYN&compact=ultra&apiKey={api}')
+rub = requests.get(f'https://free.currconv.com/api/v7/convert?q=UAH_RUB&compact=ultra&apiKey={api}')
+usd = requests.get(f'https://free.currconv.com/api/v7/convert?q=UAH_USD&compact=ultra&apiKey={api}')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345'
+
+
+@app.route('/')
+def index():
+    info_bun = byn.json()['UAH_BYN']
+    info_rub = rub.json()['UAH_RUB']
+    info_usd = usd.json()['UAH_USD']
+    return render_template('main.html', byn=(int((info_bun * 40) * 100) / 100), rub=(int((info_rub * 40) * 100) / 100),
+                           usd=(int((info_usd * 40) * 100) / 100))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5007)
+    app.run()
